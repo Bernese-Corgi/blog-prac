3e0ae4912585490460179acaccc966e542b86d6a
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-29 01:17:47 +0900

    회원 인증 API 라우트 구조 생성

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,10 +1,12 @@
 import Router from 'koa-router';
 import posts from './posts';
+import auth from './auth';
 
 const api = new Router();
 
 /* -------------------------------- 라우트 불러오기 -------------------------------- */
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 /* -------------------------------- 라우터 내보내기 ------------------------------- */
 export default api;
