b86055e25dc0fe1f202420217a5b05e32a996ffe
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-29 01:03:04 +0900

    User 모델 메서드 및 정적 메서드 작성

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -7,6 +7,39 @@ const UserSchema = new Schema({
   hashedPassword: String,
 });
 
+/* -------------------------------- 인스턴스 메서드 -------------------------------- */
+/** setPassword(password) (커스텀 인스턴스 메서드)
+ * 비밀번호를 인수로 받아 계정의 hashedPassword 값 설정
+ * @param password 해싱할 비밀번호
+ */
+UserSchema.methods.setPassword = async function (password) {
+  // 함수 내부에서 this에 접근해야 하기 때문에 function 키워드 사용해야 한다.
+  const hash = await bcrypt.hash(password, 10);
+  // this : 문서 인스턴스
+  this.hashedPassword = hash;
+};
+
+/** checkPassword(password) (커스텀 인스턴스 메서드)
+ * 인수로 받은 비밀번호가 해당 계정의 비밀번호와 일치하는지 검증
+ * @param password 해당 계정과 일치하는지 확인할 비밀번호
+ * @returns boolean
+ */
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result; // true or false
+};
+
+/* --------------------------------- 정적 메서드 --------------------------------- */
+/** findByUsername(username)
+ * username으로 데이터를 찾을 수 있다
+ * @param username 데이터를 찾을 username
+ * @returns 데이터
+ */
+UserSchema.statics.findByUsername = function (username) {
+  // this : 모델 (User)
+  return this.findOne({ username });
+};
+
 /* ---------------------------------- 모델 생성 --------------------------------- */
 const User = mongoose.model('User', UserSchema);
 
