612ff6656abe90826048b52eebe20d0bdd958d46
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-02 04:21:40 +0900

    front-end: redux 샘플 모듈 작성 및 적용

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
+import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
 
 reportWebVitals();
