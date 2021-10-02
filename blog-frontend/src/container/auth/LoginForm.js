402eea05dc6c887acb9c4a50232d6bf11be303e6
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-02 14:38:46 +0900

    front-end: 로그인 구현

--- blog-frontend/src/container/auth/LoginForm.js
+++ blog-frontend/src/container/auth/LoginForm.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { withRouter } from 'react-router';
 import { AuthForm } from '../../components/auth';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
+import { check } from '../../modules/user';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
   const dispatch = useDispatch();
 
-  const { form } = useSelector(({ auth }) => ({ form: auth.login }));
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+    form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
+  }));
 
   /* input 변경 이벤트 핸들러 ---------------------------- */
   const onChange = (e) => {
@@ -17,6 +24,10 @@ const LoginForm = () => {
   /* 폼 등록 이벤트 핸들러 ------------------------------ */
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const { username, password } = form;
+    // onSubmit 이벤트가 발생했을 때 login 함수에 현재 username과 password를 파라미터로 넣어 액션을 디스패치
+    dispatch(login({ username, password }));
   };
 
   /* form 초기화 -------------------------------- */
@@ -24,6 +35,29 @@ const LoginForm = () => {
     // 컴포넌트 처음 렌더링될 때 form을 초기화
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  /* 로그인 성공/실패 처리 ------------------------------ */
+  useEffect(() => {
+    // authError 값이 유효한 경우
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    // auth 값이 유효한 경우
+    if (auth) {
+      console.log('로그인 성공');
+      console.log(auth);
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  /* user 값이 성공적으로 설정되면 메인 화면으로 이동 --------------------- */
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [user, history]);
 
   return (
     <AuthForm
@@ -35,4 +69,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
