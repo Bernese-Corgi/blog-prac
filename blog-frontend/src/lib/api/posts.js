ef74f06ad81a98832b49e5f7035a2b5180922294
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-04 03:01:46 +0900

    front-end: 포스트 API 연동 - 특정 포스트 읽기 API 요청 함수 작성

--- blog-frontend/src/lib/api/posts.js
+++ blog-frontend/src/lib/api/posts.js
@@ -3,3 +3,6 @@ import client from './client';
 /* 포스트 작성 --------------------------------- */
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
+
+/* 특정 포스트 조회 ------------------------------- */
+export const readPost = (id) => client.get(`/api/posts/${id}`);
