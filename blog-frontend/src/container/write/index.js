ff535337809936c588a55170da65cc472896eb6a
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-04 02:23:20 +0900

    front-end: 글쓰기 API 연동 - WriteActionButtonsContainer 컴포넌트 작성 (포스트 등록/취소)

--- blog-frontend/src/container/write/index.js
+++ blog-frontend/src/container/write/index.js
@@ -1,2 +1,3 @@
 export { default as EditorContainer } from './EditorContainer';
 export { default as TagBoxContainer } from './TagBoxContainer';
+export { default as WriteActionButtonsContainer } from './WriteActionButtonsContainer';
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Responsive } from '../components/common';
-import { WriteActionButtons } from '../components/write';
-import { EditorContainer, TagBoxContainer } from '../container/write';
+import {
+  EditorContainer,
+  TagBoxContainer,
+  WriteActionButtonsContainer,
+} from '../container/write';
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
       <TagBoxContainer />
-      <WriteActionButtons />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
