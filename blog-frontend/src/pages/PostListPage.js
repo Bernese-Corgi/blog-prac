c5a3d95dba5928304a3bc7382f36a27262c5a777
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-03 18:08:44 +0900

    front-end: 로그인 상태 보여주기

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Header } from '../components/common';
+import { HeaderContainer } from '../container/common';
 
 const PostListPage = () => {
   return (
     <>
-      <Header />
+      <HeaderContainer />
       <div>안녕하세요.</div>
     </>
   );
