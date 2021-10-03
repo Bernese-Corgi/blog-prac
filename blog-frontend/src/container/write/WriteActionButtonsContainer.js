1b91eab177ca79f1f9a8192f69022f47cd3df067
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-04 02:31:29 +0900

    front-end: 글쓰기 API 연동 - WriteActionButtonsContainer 설명 추가

--- blog-frontend/src/container/write/WriteActionButtonsContainer.js
+++ blog-frontend/src/container/write/WriteActionButtonsContainer.js
@@ -17,18 +17,23 @@ const WriteActionButtonsContainer = ({ history }) => {
 
   /* 포스트 등록 --------------------------------- */
   const onPublish = () => {
+    // 리덕스 스토어 안에 들어 있는 값 사용해 새 포스트 작성
     dispatch(writePost({ title, body, tags }));
   };
 
   /* 포스트 등록 취소 ------------------------------- */
   const onCancel = () => {
+    // 취소 버튼을 누르면 브라우저에서 뒤로 가기
     history.goBack();
   };
 
   /* 성공 혹은 실패 시 할 작업 ---------------------------- */
   useEffect(() => {
+    // 포스트 작성 성공
     if (post) {
+      // 서버에서 응답한 포스트 정보의 _id, user 값을 참조
       const { _id, user } = post;
+      // 서버에서 응답한 포스트의 경로로 이동
       history.push(`/@${user.username}/${_id}`);
     }
     if (postError) {
