d83f8a262d7592b7f009b410d722d79b88319e22
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-06 01:37:38 +0900

    back-end: koa-static으로 서버 정적 파일 제공

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -5,6 +5,9 @@ import bodyParser from 'koa-bodyparser';
 import api from './api';
 import mongoose from 'mongoose';
 import jwtMiddleware from './lib/jwtMiddleware';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 /* ------------------------------- 환경 변수 파일 참조 ------------------------------ */
 // process.env 내부 값에 대한 레퍼런스
@@ -37,6 +40,19 @@ app.use(jwtMiddleware);
 
 /* app 인스턴스에 라우터 적용 ---------------------------- */
 app.use(router.routes()).use(router.allowedMethods());
+
+/* ------------------------------- koa-static ------------------------------- */
+// blog-frontend/build 디렉터리의 파일들을 서버를 통해 조회
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  // send 미들웨어 : 클라이언트 기반 라우팅이 제대로 작동하게 한다.
+  // 1. Not Found이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // 2. index.html 내용을 반환
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 /* ---------------------------------- 포트 지정 --------------------------------- */
 const port = PORT || 4000; // PORT가 지정되어 있지 않다면 4000을 사용
